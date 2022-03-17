e3f54bdc3744cd7aa4be70e7df413e2a0615f1f3
rintaro.sawada <rinwashi321@gamil.com>
2022-03-17 16:43:34 +0000

    kadai-tasklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_103446) do
+ActiveRecord::Schema.define(version: 2022_03_17_160044) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
