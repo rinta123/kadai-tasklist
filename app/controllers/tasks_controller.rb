6daf2cab12c8efa0f5029120477ae71d79440eb0
rintaro.sawada <rinwashi321@gamil.com>
2022-03-20 09:26:56 +0000

    users

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
- 
+  before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @tasks = Task.all
   end
@@ -14,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
